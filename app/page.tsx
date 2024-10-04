95198202aab35afe877461711567a36066bc5fd2
Sereth1 <papakias444@gmail.com>
2024-10-04 17:53:04 +0800

    removed train

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
             <Drifting />
             <TimelineDemo />
             <ChristosTeam />
-            <CarouzelTraining />
+            {/* <CarouzelTraining /> */}
             <ParallaxScrollMain />
 
             <Footer />
